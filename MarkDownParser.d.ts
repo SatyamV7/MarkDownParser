3d5f11202f4896c9a8cb3165c58d885bdde77b33
Satyam Verma <satyamverma46@outlook.com>
2024-03-07 19:55:12 +0530

    Initial commit

---
+++ MarkDownParser.d.ts
@@ -0,0 +1,7 @@
+/**
+ * Parses a provided Markdown string into valid HTML.
+ *
+ * @param  {string} string Markdown input for transformation
+ * @return {string}        Transformed HTML output
+ */
+export function parse(string: string): string;
